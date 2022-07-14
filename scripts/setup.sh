d63c8070a51fe48c7acd6fe5369cdc300598d1e4
Axel Larsson <iamaxellarsson@gmail.com>
2022-07-14 10:58:00 +0200

    Fixeed Glaros down issue

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -39,7 +39,9 @@ fi
 cd $LIB_DIR
 if [ ! -d "parmetis-4.0.3" ]; then
 
-  wget http://glaros.dtc.umn.edu/gkhome/fetch/sw/parmetis/parmetis-4.0.3.tar.gz
+  # wget http://glaros.dtc.umn.edu/gkhome/fetch/sw/parmetis/parmetis-4.0.3.tar.gz
+  wget https://github.com/KarypisLab/ParMETIS/archive/main.tar.gz
+  mv main.tar.gz parmetis-4.0.3.tar.gz # Rename ParMETIS file. Bit of a lazy fix...
   tar -zxvf parmetis-4.0.3.tar.gz
   cd parmetis-4.0.3
   make config
